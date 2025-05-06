dea66d07d48b4b59c78d1d16b99f69fcd52d449b
omarfciid <omarfc@iideac.edu.mx>
2025-05-05 18:23:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,19 +11,21 @@ def validar_curp(curp):
 # Conectar a Google Sheets usando las credenciales
 def conectar_sheets():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    [gcp_service_account]
-type = "service_account"
-project_id = "botiideac"
-private_key_id = "7726870f309a5183af0ebc0e132a717941f5a76c"
-private_key = """-----BEGIN PRIVATE KEY-----MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC6j92F4B1ihkqjiUuvpr3rPZABIzczSoH83KtM/EV9PGeS93AqsvFWW6g9f6k/02MeF7iQVmMPGFODYkzSO+QFilmwnti2ZZk+HUlWKwr53LwNRRfjanxa0REvIV/QPbDROfCAwovhd0HUUHU3m/BhmshVbLLw2pFa5HQo7Zvf9fsZtpSEtNm/bpb+Lt2Ugyt66jGjZekNjIktDlLcj5EqO8PqWUuyJeLgpAvhaSf3bMMVwDx2zuGRn7ha57Vqz90Xem06MgTzj7c/wLJ7PbZcLXnFS4ioI49RxzPMnsaDwrt8W4LpadgLwdpO0Q+UBi/qONCJiYkOMOfnLkivVm+PAgMBAAECggEAAoMebgeTDbkWNzaJlN9vbkQBdOp2+1NVBcUo71v0SZfvIxz+3yxSsKGCVfWqn0fm7UhPfW4UXmcq9Ll1pF0XGHYA+C5jZ61dj2cQjj79Ilk1FQ6poC/WZdir3REbe6/g+ihIE0vLW2liMww0T9/jMEVHzlKs5C5lR66M2+I7N5xP9RWQ6PHYlJ49xMo7EpMAU9iv6qmgG7DIzsvM+tVq7z2HU0z6HXRLEfGFZodV4yPdwlDYLyzTzoiTRcmtGhYmMXZVjn6BIPUZs/hNBYi3dNwr4e5lXsiKXiQ0jONb0qtmDcdXmYdqdodu8z/uiuM7g1AoHMyqFe4s0vFiMvzybQKBgQDmCTY847G1j6aF2azxx2DY+PbhbWs11nZ6pXaLadE/O/+Cyryz+6hBRvjVg+58Rq//h5lgONaxxse6xf92wCMX16ak00UAd7Zu+jRskyhOlXgFhUm750qcx6OJpU9H4x8fRAt2FNYELoSGy275hHo1Z9PLRg7U6/YA6mNKzG38SwKBgQDPnn27tITNUQxFDwqXTFCejdb0JwcYXPkxm0mFajc1KpVvu0yy2xFsJ+3VBvckOk8kGcw1cAF5g6l89/xwDMu6e93uKbNBRIVmtc9JABdwT7yix2VbRy7EzGcNxUnG2bi4ve1Pu3sHFIfiU7LKQYG1mdegLoNLYrLHVX+saOKHTQKBgB1cfaTF90/SVxHsl8uAhFcNaA2b9rw3dIXxCrF+vyuQD+v0zqM2cuJqGaa/ITiRmTNHboc+mgC0+5dWYGYa3h/T8bxGDx+hKBMAMqLNIa7uGxSxT5kBS5+5R6FxDS6Cyd0mxUO8IxkDivzdiHumsiaQ6xKeDZSVzZ+OS0An2b4DAoGBAL1wKd1Za0tDxmREwy8l7PGfDiEcczbhrmZ7AGyBa/pQ7qVSSztu88ix7ipP/rTJl2ijOVcQ6eeMINvsEiPTKRjw6KDk37CP6wC6p3Y+ZSSPPNlyAqN0odHjh6xi9VxBUHEQqzXzJOBi9VGneR1fBBFFXB/GEPp6BhIniyUvVnKJAoGAT6nZ3imCaFj1aZ+uL5Rk8CXmxD52J2Lv4o3F6PUinuZ0tdVAe3QZ51mAYDAd7SiRsohW6uPQH8rupZqrsxgNq4D7VBihc1xObnPKS2ub6iEWmG+2wK+40d5bc2LlC/CFtRXujh+MtUfo4ofkRBcemYIsdUvk6lo7RNv3cqeZ8pE=-----END PRIVATE KEY-----"""
-client_email = "botiideac@botiideac.iam.gserviceaccount.com"
-client_id = "106818133155189707697"
-auth_uri = "https://accounts.google.com/o/oauth2/auth"
-token_uri = "https://oauth2.googleapis.com/token"
-auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs"
-client_x509_cert_url = "https://www.googleapis.com/robot/v1/metadata/x509/botiideac%40botiideac.iam.gserviceaccount.com"
-universe_domain = "googleapis.com"
-
+    creds = ServiceAccountCredentials.from_json_keyfile_dict({
+        "type": "service_account",
+        "project_id": "botiideac",
+        "private_key_id": "7726870f309a5183af0ebc0e132a717941f5a76c",
+        "private_key": """-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC6j92F4B1ihkqjiUuvpr3rPZABIzczSoH83KtM/EV9PGeS93AqsvFWW6g9f6k/02MeF7iQVmMPGFODYkzSO+QFilmwnti2ZZk+HUlWKwr53LwNRRfjanxa0REvIV/QPbDROfCAwovhd0HUUHU3m/BhmshVbLLw2pFa5HQo7Zvf9fsZtpSEtNm/bpb+Lt2Ugyt66jGjZekNjIktDlLcj5EqO8PqWUuyJeLgpAvhaSf3bMMVwDx2zuGRn7ha57Vqz90Xem06MgTzj7c/wLJ7PbZcLXnFS4ioI49RxzPMnsaDwrt8W4LpadgLwdpO0Q+UBi/qONCJiYkOMOfnLkivVm+PAgMBAAECggEAAoMebgeTDbkWNzaJlN9vbkQBdOp2+1NVBcUo71v0SZfvIxz+3yxSsKGCVfWqn0fm7UhPfW4UXmcq9Ll1pF0XGHYA+C5jZ61dj2cQjj79Ilk1FQ6poC/WZdir3REbe6/g+ihIE0vLW2liMww0T9/jMEVHzlKs5C5lR66M2+I7N5xP9RWQ6PHYlJ49xMo7EpMAU9iv6qmgG7DIzsvM+tVq7z2HU0z6HXRLEfGFZodV4yPdwlDYLyzTzoiTRcmtGhYmMXZVjn6BIPUZs/hNBYi3dNwr4e5lXsiKXiQ0jONb0qtmDcdXmYdqdodu8z/uiuM7g1AoHMyqFe4s0vFiMvzybQKBgQDmCTY847G1j6aF2azxx2DY+PbhbWs11nZ6pXaLadE/O/+Cyryz+6hBRvjVg+58Rq//h5lgONaxxse6xf92wCMX16ak00UAd7Zu+jRskyhOlXgFhUm750qcx6OJpU9H4x8fRAt2FNYELoSGy275hHo1Z9PLRg7U6/YA6mNKzG38SwKBgQDPnn27tITNUQxFDwqXTFCejdb0JwcYXPkxm0mFajc1KpVvu0yy2xFsJ+3VBvckOk8kGcw1cAF5g6l89/xwDMu6e93uKbNBRIVmtc9JABdwT7yix2VbRy7EzGcNxUnG2bi4ve1Pu3sHFIfiU7LKQYG1mdegLoNLYrLHVX+saOKHTQKBgB1cfaTF90/SVxHsl8uAhFcNaA2b9rw3dIXxCrF+vyuQD+v0zqM2cuJqGaa/ITiRmTNHboc+mgC0+5dWYGYa3h/T8bxGDx+hKBMAMqLNIa7uGxSxT5kBS5+5R6FxDS6Cyd0mxUO8IxkDivzdiHumsiaQ6xKeDZSVzZ+OS0An2b4DAoGBAL1wKd1Za0tDxmREwy8l7PGfDiEcczbhrmZ7AGyBa/pQ7qVSSztu88ix7ipP/rTJl2ijOVcQ6eeMINvsEiPTKRjw6KDk37CP6wC6p3Y+ZSSPPNlyAqN0odHjh6xi9VxBUHEQqzXzJOBi9VGneR1fBBFFXB/GEPp6BhIniyUvVnKJAoGAT6nZ3imCaFj1aZ+uL5Rk8CXmxD52J2Lv4o3F6PUinuZ0tdVAe3QZ51mAYDAd7SiRsohW6uPQH8rupZqrsxgNq4D7VBihc1xObnPKS2ub6iEWmG+2wK+40d5bc2LlC/CFtRXujh+MtUfo4ofkRBcemYIsdUvk6lo7RNv3cqeZ8pE=-----END PRIVATE KEY-----"""
+        "client_email": "botiideac@botiideac.iam.gserviceaccount.com",
+        "client_id": "106818133155189707697",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/botiideac%40botiideac.iam.gserviceaccount.com",
+        "universe_domain": "googleapis.com"
+    }, scope)
+    
     client = gspread.authorize(creds)
     return client.open("FAQ")  # Cambia aquí si tu documento tiene otro nombre
 
