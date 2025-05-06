f1b8c6b2a715b2598205cc665f7f68a90aeb689d
omarfciid <omarfc@iideac.edu.mx>
2025-05-05 18:37:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import re
-
-# Función para validar CURP
-#def validar_curp(curp):
- #   patron = r"^[A-Z]{4}\d{6}[HM][A-Z]{5}[A-Z0-9]\d$"
-  #  return re.match(patron, curp.upper()) is not None
 
 # Conexión a Google Sheets usando credenciales desde secrets.toml
 def conectar_sheets():
@@ -29,22 +23,18 @@ def chatbot():
     st.title("Chatbot del Curso")
 
     nombre = st.text_input("¿Cuál es tu nombre?")
-   # curp = st.text_input("Introduce tu CURP:")
     pregunta = st.text_input("¿Qué te gustaría saber sobre el curso?")
 
     if st.button('Preguntar'):
-        if not validar_curp(curp):
-            st.error("El CURP proporcionado no es válido. Por favor, verifica.")
-        else:
-            faq_dict = cargar_faq()
-            pregunta_lower = pregunta.strip().lower()
-            respuesta = faq_dict.get(pregunta_lower, "No entiendo la pregunta. ¿Podrías reformularla?")
-            st.write(f"Respuesta: {respuesta}")
+        faq_dict = cargar_faq()
+        pregunta_lower = pregunta.strip().lower()
+        respuesta = faq_dict.get(pregunta_lower, "No entiendo la pregunta. ¿Podrías reformularla?")
+        st.write(f"Respuesta: {respuesta}")
 
-            # Guardar datos del usuario en la hoja "Usuarios"
-            documento = conectar_sheets()
-            hoja_usuarios = documento.worksheet("Usuarios")
-            hoja_usuarios.append_row([nombre, curp.upper(), pregunta, respuesta])
+        # Guardar datos del usuario en la hoja "Usuarios"
+        documento = conectar_sheets()
+        hoja_usuarios = documento.worksheet("Usuarios")
+        hoja_usuarios.append_row([nombre, pregunta, respuesta])
 
 # Ejecutar la app
 if __name__ == '__main__':
